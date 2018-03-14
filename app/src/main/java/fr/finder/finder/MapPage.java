888e2daeb1b9292422512d033ba8b1b55d3a1f06
GiJiang <gillesjiang1@gmail.com>
2018-03-14 23:27:52 +0100

    add method getLocation by adress and design activity

--- app/src/main/java/fr/finder/finder/MapPage.java
+++ app/src/main/java/fr/finder/finder/MapPage.java
@@ -1,6 +1,10 @@
 package fr.finder.finder;
 
 
+import android.content.Context;
+import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
@@ -15,7 +19,11 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
 
 
 /**
@@ -62,10 +70,44 @@ public class MapPage extends Fragment implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
-        LatLng pos = new LatLng(11.111,-11.111);
+        LatLng pos = getLocationFromAddress(getActivity(), "Paris, France");
+
         MarkerOptions option = new MarkerOptions();
-        option.position(pos).title("salut");
+        option.position(pos).title("Paris");
         map.addMarker(option);
         map.moveCamera(CameraUpdateFactory.newLatLng(pos));
+        map.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
+            @Override
+            public void onInfoWindowClick(Marker marker) {
+                //Intent intent = new Intent(getActivity(), PageCompany.class);
+                //startActivity(intent);
+            }
+        });
     }
+
+    public void customAddMarker(LatLng latLng, String title, String snippet) {
+        //use when we will custom the marker
+    }
+
+    public LatLng getLocationFromAddress(Context context,String strAddress) {
+        Geocoder coder = new Geocoder(context);
+        List<Address> address;
+        LatLng position = null;
+        try {
+            address = coder.getFromLocationName(strAddress, 2);
+            if (address == null) {
+                return null;
+            }
+            Address location = address.get(0);
+            location.getLatitude();
+            location.getLongitude();
+            position = new LatLng(location.getLatitude(), location.getLongitude() );
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        return position;
+    }
+
 }
